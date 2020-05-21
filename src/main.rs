1b71aa568bf63ad76688fcc4390ab493c619d1b2
joemclo8 <joemclo8@gmail.com>
2020-05-21 23:33:28 +0300

    Lint fix

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,41 @@
-// #![deny(unsafe_code)]
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
-
 
 #[allow(unused_extern_crates)]
 extern crate panic_halt;
 #[macro_use(block)]
 extern crate nb;
 
+use core::f32::consts::PI;
 use cortex_m;
 use cortex_m_rt::entry;
 use f3::{
-    hal::{i2c::I2c, delay::Delay, prelude::*, stm32f30x, serial::Serial},
-    led::{Leds, Direction},
+    hal::{
+        delay::Delay,
+        i2c::I2c,
+        prelude::*,
+        serial::{self, Serial},
+        stm32f30x,
+    },
+    led::{Direction, Leds},
     lsm303dlhc::I16x3,
-    Lsm303dlhc
+    Lsm303dlhc,
 };
-use core::f32::consts::PI;
 use numtoa::NumToA;
 
 #[allow(unused_imports)]
 use m::Float;
 
+type I2C = f3::hal::i2c::I2c<
+    stm32f30x::I2C1,
+    (
+        f3::hal::gpio::gpiob::PB6<f3::hal::gpio::AF4>,
+        f3::hal::gpio::gpiob::PB7<f3::hal::gpio::AF4>,
+    ),
+>;
 
-#[entry]
-fn main() -> ! {
+fn setup() -> (Delay, serial::Tx<stm32f30x::USART1>, f3::led::Leds, I2C) {
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = stm32f30x::Peripherals::take().unwrap();
 
@@ -35,27 +46,32 @@ fn main() -> ! {
 
     // setup leds
     let gpioe = dp.GPIOE.split(&mut rcc.ahb);
-    let mut leds = Leds::new(gpioe);
-
+    let leds = Leds::new(gpioe);
 
     // setup i2c for magnometer
     let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
     let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
     let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
     let i2c = I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
-    let mut lsm303dlhc = Lsm303dlhc::new(i2c).unwrap();
-
 
     // setup usart interface
     let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
     let tx = gpioa.pa9.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
     let rx = gpioa.pa10.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
     let a = Serial::usart1(dp.USART1, (tx, rx), 9600.bps(), clocks, &mut rcc.apb2);
-    let (mut tx, _) = a.split();
+    let (tx, _) = a.split();
 
     // setup delay interface
-    let mut delay = Delay::new(cp.SYST, clocks);
+    let delay = Delay::new(cp.SYST, clocks);
 
+    (delay, tx, leds, i2c)
+}
+
+#[entry]
+fn main() -> ! {
+    let (mut delay, mut tx, mut leds, i2c) = setup();
+
+    let mut lsm303dlhc = Lsm303dlhc::new(i2c).unwrap();
 
     leds[Direction::North].on();
     leds[Direction::South].on();
@@ -64,14 +80,11 @@ fn main() -> ! {
 
     // infinite loop;
     loop {
-
-
-        let I16x3 {x, y, z:_} = lsm303dlhc.mag().unwrap();
-
+        let I16x3 { x, y, z } = lsm303dlhc.mag().unwrap();
 
         let theta = (y as f32).atan2(x as f32);
 
-        let  dir = if theta < -7. * PI / 8. {
+        let dir = if theta < -7. * PI / 8. {
             Direction::North
         } else if theta < -5. * PI / 8. {
             Direction::Northwest
@@ -94,29 +107,57 @@ fn main() -> ! {
         leds.iter_mut().for_each(|led| led.off());
         leds[dir].on();
 
-        let mut x_buffer = [0u8; 20];
-        x.numtoa(10, &mut x_buffer);
+        let mut buffer_x = [0u8; 10];
+        let mut buffer_y = [0u8; 10];
+        let mut buffer_z = [0u8; 10];
 
-        let mut y_buffer = [0u8; 20];
-        y.numtoa(10, &mut y_buffer);
+        x.numtoa(10, &mut buffer_x);
+        y.numtoa(10, &mut buffer_y);
+        z.numtoa(10, &mut buffer_z);
 
+        let mut buffer = [0u8, 30];
+
+        let mut index = 0;
+        for byte in buffer_x.iter_mut() {
+            buffer[index] = *byte;
+
+            index += 1;
+        }
+        for byte in buffer_y.iter_mut() {
+            buffer[index] = *byte;
+
+            index += 1;
+        }
+        for byte in buffer_z.iter_mut() {
+            buffer[index] = *byte;
+
+            index += 1;
+        }
+
+        // buffer[0..19].copy_from_slice(&buffer_x);
+        // buffer[20..39].copy_from_slice(&buffer_y);
+        // buffer[40..59].copy_from_slice(&buffer_z);
 
         // write to usart, block until sent
-        for byte in x_buffer.iter_mut() {
+        for byte in buffer.iter_mut() {
             block!(tx.write(*byte)).ok();
         }
 
         block!(tx.write(b'\t')).ok();
 
-        for byte in y_buffer.iter_mut() {
-            block!(tx.write(*byte)).ok();
-        }
+        // for byte in y_buffer.iter_mut() {
+        //     block!(tx.write(*byte)).ok();
+        // }
 
-        for byte in b"\r\n" {
-            block!(tx.write(*byte)).ok();
-        }
+        // block!(tx.write(b'\t')).ok();
+
+        // for byte in z_buffer.iter_mut() {
+        //     block!(tx.write(*byte)).ok();
+        // }
+
+        block!(tx.write(b'\n')).ok();
 
         // delay for 100ms
-        delay.delay_ms(100_u16);
+        // delay.delay_ms(100_u16);
     }
 }
